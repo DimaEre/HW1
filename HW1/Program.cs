ac7c122b68e40cf98e85333dd0540377940e7972
Дмитро Єремія <diameremia2006@gmail.com>
2022-10-15 11:50:32 +0300

    Add project files.

---
+++ HW1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{ 
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 36; i++)
+        {
+            for(int j = 1; j <= 36; j++)
+            {
+                for(int x = 1; x <= 36; x++)
+                {
+                    if(i + j +x == 13 && i*j*x == 36)
+                    {
+                        Console.WriteLine(i + " " + x + " " + j + " ");
+                    }
+                }
+            }
+        }
+    }
+}
